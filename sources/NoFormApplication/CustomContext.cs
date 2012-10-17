a437bd33cb806d42a03ec5d0f93f3ee8635dd32e
marckm <maxime.marck@gmail.com>
2012-10-17 12:11:10 +0200

    added inital project
    Contains implementation to start a customcontext instance and display a
    notify icon in tray bar.

---
+++ sources/NoFormApplication/CustomContext.cs
@@ -0,0 +1,64 @@
+﻿namespace NoFormApplication
+{
+    using System;
+    using System.Windows.Forms;
+    using NoFormApplication.Properties;
+
+    /// <summary>
+    /// Application specific context used to create application's main standard loop without form.
+    /// </summary>
+    /// <see cref="http://msdn.microsoft.com/en-us/library/ms157901.aspx" />
+    public class CustomContext : ApplicationContext
+    {
+        /// <summary>
+        /// Used to display application icon in taskbar and interact with user.
+        /// </summary>
+        private NotifyIcon trayIcon;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CustomContext" /> class.
+        /// </summary>
+        public CustomContext()
+        {
+            this.BuildNotifyIconInstance();
+        }
+
+        /// <summary>
+        /// Builds the tray NotifyIcon instance.
+        /// </summary>
+        private void BuildNotifyIconInstance()
+        {
+            // tray icon initialization
+            this.trayIcon = new NotifyIcon()
+            {
+                Text = "Application with no form",
+                Icon = Resources.TrayIcon
+            };
+
+            // creates a context menu with one Exit item.
+            var contextMenu = new ContextMenuStrip();
+            var exitMenuItem = new ToolStripMenuItem("Exit");
+            contextMenu.Items.Add(exitMenuItem);
+            exitMenuItem.Click += this.Exit;
+
+            // we assign the context menu to the tray icon object
+            this.trayIcon.ContextMenuStrip = contextMenu;
+
+            // makes the tray icon visible
+            this.trayIcon.Visible = true;
+        }
+
+        /// <summary>
+        /// Handles the Exit menu item click.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="EventArgs" /> instance containing the event data.</param>
+        private void Exit(object sender, EventArgs e)
+        {
+            // Hide tray icon, otherwise it will remain shown until user mouses over it
+            trayIcon.Visible = false;
+
+            Application.Exit();
+        }
+    }
+}
---
+++ sources/NoFormApplication/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace NoFormApplication
+{
+    using System;
+    using System.Windows.Forms;
+
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // do all your stuff here... (run a background thread, host a WCF service...)
+            Application.ApplicationExit += OnApplicationExit;
+            AppDomain.CurrentDomain.UnhandledException += OnCurrentDomainUnhandledException;
+
+            Application.Run(new CustomContext());
+        }
+
+        /// <summary>
+        /// Do here what needs to be done when application closes
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="EventArgs" /> instance containing the event data.</param>
+        static void OnApplicationExit(object sender, EventArgs e)
+        {
+            // close here all your background services (thread, WCF service...)
+        }
+
+        /// <summary>
+        /// Called when an unhandled exception appears.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="System.UnhandledExceptionEventArgs" /> instance containing the event data.</param>
+        private static void OnCurrentDomainUnhandledException(object sender, System.UnhandledExceptionEventArgs e)
+        {
+            // we come here if there is any exception not handled correctly (i.e. in CustomContext).
+            // normally we should log, display error to user and exit the app.
+            System.Environment.Exit(0);
+        }
+    }
+}
